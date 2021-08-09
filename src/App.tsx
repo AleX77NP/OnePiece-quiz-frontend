7274b57831620aa0ca8d07ebc334f6f00e836173
Aleksandar Milanovic <milanovicaleX77@gmail.com>
2021-08-09 15:09:12 +0200

    prepare for deploy...

--- src/App.tsx
+++ src/App.tsx
@@ -29,7 +29,7 @@ const App: React.FC = () => {
   const authState = useAppSelector(selectAuth)
   const dispatch = useAppDispatch()
 
-  console.log(authState.auth)
+  //console.log(authState.auth)
 
   useEffect(() => {
     const fetchUser = async() => {
